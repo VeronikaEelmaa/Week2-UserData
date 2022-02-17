6491ca1590dc31298d61088fb1900f21bcfca244
Veronika Eelmaa <veronika.eelmaa@gmail.com>
2022-02-17 22:51:13 +0200

    Control Flow

---
+++ UserAge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaasta;
+            //programm kuvab sünniaasta konsoolis;
+            //"oled sündinud {yearOfBirth}";
+
+            Console.WriteLine("Palun sisesta oma vanus:");
+            int userAge= Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2021 - userAge;
+            Console.WriteLine($"Oled sündinud {yearOfBirth} .aastal.");
+
+        }
+    }
+}
